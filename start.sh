8941508243aa6a3d2a17ccaf307aa460581cfc1b
G111ANT <74424409+G111ANT@users.noreply.github.com>
2025-10-04 15:21:43 -0400

    UV might not be installed

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-uv run --prerelease=if-necessary-or-explicit main.py
+(uv run --prerelease=if-necessary-or-explicit main.py || pipx run uv run --prerelease=if-necessary-or-explicit main.py)
